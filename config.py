c818519ef66c12a474093eaea33e03cbc957ae1a
Igor2sia <91655233+Igor2sia@users.noreply.github.com>
2022-03-03 17:28:18 +0300

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-settings = {
-    'CLIENT_ID':'TjQUy30mMI8D0UADjZip7A',
-    'SECRET_CODE':'eDBwFzv1mvqItb0QvDqBXXR_GTMphg',
-    'DISCORD_TOKEN':'ODkzMTg4Mjg5MjcwNTQ2NTEy.YVX0ag.uiADAA678k3T3jbKLnX6LDslvn4'
-}
